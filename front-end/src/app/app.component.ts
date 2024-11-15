4f127928cc76b648ce76046a4bcd52b54e3999ef
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-11-15 16:40:08 +0300

    appcomponent simplified

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule, Router, NavigationEnd } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { filter } from 'rxjs/operators';
-import { LayoutModule } from './layout/layout.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule, LayoutModule]
+  imports: [CommonModule, RouterModule]
 })
 export class AppComponent {
-  isAuthRoute: boolean = false;
-
-  constructor(private router: Router) {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: any) => {
-      this.isAuthRoute = event.url.startsWith('/auth');
-    });
-  }
 }
